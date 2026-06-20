d884ca6ef2844ad96358aeb3d3c1be05792ba454
Shivam Birla <122011817+shivambirla139@users.noreply.github.com>
2026-06-20 09:42:03 +0530

    Add Python template with input/output handling

---
+++ Python Template.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import heapq
+
+from math import *
+from collections import deque, Counter, defaultdict
+from bisect import bisect_left, bisect_right
+from itertools import permutations, combinations, accumulate
+
+if os.path.exists("in.txt"):
+    sys.stdin = open("in.txt", "r")
+    sys.stdout = open("out.txt", "w")
+
+input = sys.stdin.readline
+
+INF = 10**18
+MOD = 10**9 + 7
+
+
+def solve():
+    pass
+
+
+t = int(input())
+
+for _ in range(t):
+    solve()
