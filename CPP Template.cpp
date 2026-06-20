86b4d575bec1b86e8ea55108276b9bd605c7fc9e
Shivam Birla <122011817+shivambirla139@users.noreply.github.com>
2026-06-20 09:35:51 +0530

    Add initial CPP template with input/output handling

---
+++ CPP Template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+const int mod = 1e9 + 7, inf = 1e18, maxn = 2e5 + 5;
+void solveTestcase() {
+    
+}
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    if (fopen("in.txt", "r")) {
+        freopen("in.txt", "r", stdin);
+        freopen("out.txt", "w", stdout);
+    }
+    int t = 1;
+    // cin >> t;
+    for (int i = 1; i <= t; i++) {
+        // cout << "Test Case : " << i << endl;
+        solveTestcase();
+        cout << flush;
+    }
+}
