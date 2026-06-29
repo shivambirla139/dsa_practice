75841d97e3b969e2cc6832d4e8a0b98e614db6cf
Shivam Birla <122011817+shivambirla139@users.noreply.github.com>
2026-06-29 18:20:42 +0530

    Update CPP Template.cpp

--- CPP Template.cpp
+++ CPP Template.cpp
@@ -1,20 +1,27 @@
 #include <bits/stdc++.h>
 #define int int64_t
+#define double long double 
 using namespace std;
-const int mod = 1e9 + 7, inf = 1e18, maxn = 2e5 + 5;
 using Grid3 = vector<vector<vector<int>>>;
 using Grid2 = vector<vector<int>>;
 using Grid1 = vector<int>;
-void solveTestcase() {
-    
+
+const int mod = 1e9 + 7, inf = 1e18, maxn = 301;
+
+void solveTestcase () {
+
 }
 int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    if (fopen("in.txt", "r")) {
-        freopen("in.txt", "r", stdin);
-        freopen("out.txt", "w", stdout);
-    }
+
+    #ifndef ONLINE_JUDGE
+        if (fopen("in.txt", "r")) {
+            freopen("in.txt", "r", stdin);
+            freopen("out.txt", "w", stdout);
+        }
+    #endif
+
     int t = 1;
     // cin >> t;
     for (int i = 1; i <= t; i++) {
