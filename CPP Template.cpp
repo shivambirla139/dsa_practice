293127eafbc892958400f1abbb1510ebb0bde9b5
Shivam Birla <122011817+shivambirla139@users.noreply.github.com>
2026-07-10 17:08:28 +0530

    Refactor solveTestcase and add timing
    Updated solveTestcase function to include output message and added timing functionality.

--- CPP Template.cpp
+++ CPP Template.cpp
@@ -1,11 +1,16 @@
 #include <bits/stdc++.h>
+using namespace std;
+
 #define int int64_t
 #define double long double 
-using namespace std;
-const int mod = 1e9 + 7, inf = 1e18, maxn = 301;
-void solveTestcase () {
 
+const int mod = 1e9 + 7, inf = 1e18, maxn = 301;
+
+
+void solveTestcase() {
+    cout << "Shit works " << endl;
 }
+
 int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -21,7 +26,14 @@ int32_t main(){
     // cin >> t;
     for (int i = 1; i <= t; i++) {
         // cout << "Test Case : " << i << endl;
+        auto start = chrono::high_resolution_clock::now();
+
         solveTestcase();
+
+        auto end = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+        // cout << "Time taken: " << duration.count() << " milli Seconds\n";
+        
         // cout << endl << endl;
     }
 }
