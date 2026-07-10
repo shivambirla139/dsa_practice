d55ac94e85452e5fe52bb93f7aa9ce3f1b12297e
Shivam Birla <122011817+shivambirla139@users.noreply.github.com>
2026-07-10 17:10:09 +0530

    Refactor timing code in main function
    Removed unnecessary timing code from the loop and added timing for the entire main function.

--- CPP Template.cpp
+++ CPP Template.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 const int mod = 1e9 + 7, inf = 1e18, maxn = 301;
 
-
 void solveTestcase() {
-    cout << "Shit works " << endl;
+
 }
 
 int32_t main(){
+    
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -22,18 +22,16 @@ int32_t main(){
         }
     #endif
 
+    auto start = chrono::high_resolution_clock::now();
     int t = 1;
     // cin >> t;
     for (int i = 1; i <= t; i++) {
         // cout << "Test Case : " << i << endl;
-        auto start = chrono::high_resolution_clock::now();
-
         solveTestcase();
-
-        auto end = chrono::high_resolution_clock::now();
-        auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
-        // cout << "Time taken: " << duration.count() << " milli Seconds\n";
-        
         // cout << endl << endl;
     }
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    // cout << "Time taken: " << duration.count() << " milli Seconds\n";
+
 }
